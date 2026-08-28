7167653b755427a4012117bed8ec672fdbbe8772
Francisco Cardoso <francisco.cardoso.0@icloud.com>
2026-08-28 16:19:12 +0200

    fix decoding order

--- modules.go
+++ modules.go
@@ -6,6 +6,7 @@ import (
 	"github.com/AgentNemo00/kigo-core/information"
 	"github.com/AgentNemo00/kigo-core/inquiry"
 	"github.com/AgentNemo00/kigo-core/notification"
+	"github.com/AgentNemo00/kigo-core/order"
 	"github.com/AgentNemo00/sca-instruments/log"
 	"github.com/AgentNemo00/sca-instruments/pubsub"
 	"github.com/AgentNemo00/sca-instruments/pubsub/nats"
@@ -27,23 +28,41 @@ func GetModule(ctx context.Context, cfg *ModuleConfig) *information.ModuleInform
 		return nil
 	}
 	// pubsub suscriber
-	sub, err := nats.SubscriberWithURL[information.ModulesPayload](cfg.PubSubUrl)
+	sub, err := nats.SubscriberWithURL[order.Order](cfg.PubSubUrl)
 	if err != nil {
 		log.Ctx(ctx).Err(err)
 		return nil
 	}
 
-	received := make(chan *information.ModulesPayload)
+	errChan := make(chan error)
+	receivedChan := make(chan information.ModulesPayload)
 
-	subscb, err := sub.Subscribe(ctx, cfg.Name, func(ctx context.Context, metadata pubsub.Metadata, data *information.ModulesPayload) {
-		received <- data 
+	subscb, err := sub.Subscribe(ctx, cfg.Name, func(ctx context.Context, metadata pubsub.Metadata, data *order.Order) {
+		if metadata.Error != nil {
+			errChan <- err
+			return
+		}
+		var payload order.OrderInformationPayload
+		err := util.MapToStruct(data.Payload, &payload)
+		if err != nil {
+			errChan <- err
+			return 
+		}
+		var p information.ModulesPayload
+		err = util.MapToStruct(payload.Payload, &p)
+		if err != nil {
+			errChan <- err
+			return 
+		}
+		receivedChan <- p
 	})
 	if err != nil {
 		log.Ctx(ctx).Err(err)
 		return nil
 	}
 	defer subscb.Unsubscribe(ctx)
-	defer close(received)
+	defer close(receivedChan)
+	defer close(errChan)
 	
 	go pub.Publish(ctx, cfg.PubSubKiGo, notification.Notification{
 		From: cfg.Name,
@@ -54,32 +73,31 @@ func GetModule(ctx context.Context, cfg *ModuleConfig) *information.ModuleInform
 		},
 	})
 
-	var modules *information.ModulesPayload
-
+	var modules information.ModulesPayload
+	
 	l : for {
 		select{
 		case <- ctx.Done():
 			log.Ctx(ctx).Err(ctx.Err())
 			return nil
-		case ele, ok := <- received:
+		case ele, ok := <- receivedChan:
 			if !ok {
 				return nil
 			}
 			modules = ele
 			break l
 		default:
-
 		}
 	}
 
-	var module information.ModuleInformation
+	var module *information.ModuleInformation
 
 	for _, mod := range modules.Modules {
 		if mod.Name == cfg.NameTo {
+			module = &mod
 			break
 		}
-		module = mod
 	}
 	
-	return &module
+	return module
 }
